a449eaa39d6ed6ee67f10881b4701662882b0a96
mobrewer <morgiearan@gmail.com>
2022-10-26 07:04:55 -0600

    Morning commit, changed some styling

---
+++ end.js
@@ -0,0 +1,6 @@
+const username = document.querySelector('#username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = document.querySelector('#mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
